16be201d44d31f82f05c4fab8be02d6a6165a2d0
Langford Quarshie <langfordquarshie21@gmail.com>
2022-03-23 15:24:51 +0000

    🔃 added loader

--- components/loader.js
+++ components/loader.js
@@ -1,6 +1,19 @@
 const Loader = () => {
-    return <div className="w-screen h-screen flex items-center bg-white justify-center">
-        <p className="text-medium text-xl text-blue-600">loading...</p>
+    return <div className="w-screen h-screen flex items-center bg-white justify-center fixed left-0 top-0">
+        <div className="flex items-center justify-center h-28 w-28 pt-10 pl-5">
+            <svg>
+                <path fill="#2563EB" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+                    <animateTransform
+                        attributeName="transform"
+                        attributeType="XML"
+                        type="rotate"
+                        dur="1s"
+                        from="0 50 50"
+                        to="360 50 50"
+                        repeatCount="indefinite" />
+                </path>
+            </svg>
+        </div>
     </div>
 }
 
