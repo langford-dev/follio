f0a51e6f04f520442aca9ee4ec70a0898b256f3e
Langford Quarshie <langfordquarshie21@gmail.com>
2022-03-24 01:05:45 +0000

    🏡 bottom nav icons

--- components/bottomNavigation.js
+++ components/bottomNavigation.js
@@ -1,11 +1,39 @@
 import Link from "next/link"
+import home from "../assets/svg/home.svg"
+import brush from "../assets/svg/brush.svg"
+import pen from "../assets/svg/pen.svg"
+import menu from "../assets/svg/menu.svg"
 
 const BottomNavigation = () => {
     return <div className="sm:hidden fixed bottom-0 left-0 flex items-center justify-around w-screen bg-gray-50 font-medium p-3 border-t">
-        <Link href="/" passHref={true}>🏡</Link>
-        <Link href="/edit" passHref={true}>✍</Link>
-        <Link href="/themes" passHref={true}>🎨</Link>
-        <Link href="/" passHref={true}>🛡</Link>
+        <Link href="/" passHref={true}>
+            <div className="flex items-center flex-col">
+                <img src={home.src} />
+                <small>Home</small>
+            </div>
+        </Link>
+
+        <Link href="/edit" passHref={true}>
+            <div className="flex items-center flex-col">
+                <img src={pen.src} />
+                <small>Edit</small>
+            </div>
+        </Link>
+
+        <Link href="/themes" passHref={true}>
+            <div className="flex items-center flex-col">
+                <img src={brush.src} />
+                <small>Themes</small>
+            </div>
+        </Link>
+
+        <Link href="/" passHref={true}>
+            <div className="flex items-center flex-col">
+                <img src={menu.src} />
+                <small>More</small>
+            </div>
+        </Link>
+
     </div>
 }
 
