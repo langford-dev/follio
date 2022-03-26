3696b941f0d084b1dc328a61ae62f4bbcdc7d5bd
Langford Quarshie <langfordquarshie21@gmail.com>
2022-03-26 13:03:01 +0000

    bottom nav icons

--- components/bottomNavigation.js
+++ components/bottomNavigation.js
@@ -1,35 +1,40 @@
 import Link from "next/link"
-import home from "../assets/svg/home.svg"
-import brush from "../assets/svg/brush.svg"
-import pen from "../assets/svg/pen.svg"
-import menu from "../assets/svg/menu.svg"
+import { EditOutlined, FormatPainterOutlined, EllipsisOutlined, HomeOutlined } from '@ant-design/icons';
+
+const iconStyle = (page) => {
+    let pageUrl = window.location.pathname
+
+    if (pageUrl === page) return { fontSize: '23px', color: '#2563eb' }
+    return { fontSize: '23px' }
+
+}
 
 const BottomNavigation = () => {
     return <div className="sm:hidden fixed bottom-0 left-0 flex items-center justify-around w-screen bg-white font-medium p-3 border-t">
         <Link href="/" passHref={true}>
             <div className="flex items-center flex-col">
-                <img src={home.src} />
+                <HomeOutlined style={iconStyle("/")} />
                 <small>Home</small>
             </div>
         </Link>
 
         <Link href="/edit" pa ssHref={true}>
             <div className="flex items-center flex-col">
-                <img src={pen.src} />
+                <EditOutlined style={iconStyle("/edit")} />
                 <small>Edit</small>
             </div>
         </Link>
 
         <Link href="/themes" passHref={true}>
             <div className="flex items-center flex-col">
-                <img src={brush.src} />
+                <FormatPainterOutlined style={iconStyle("/themes")} />
                 <small>Themes</small>
             </div>
         </Link>
 
         <Link href="/" passHref={true}>
             <div className="flex items-center flex-col">
-                <img src={menu.src} />
+                <EllipsisOutlined style={iconStyle("/more")} />
                 <small>More</small>
             </div>
         </Link>
--- components/editTabs.js
+++ components/editTabs.js
@@ -4,9 +4,9 @@ import eye from "../assets/svg/eye.svg"
 import Image from "next/image"
 
 const styles = {
-    tab: `whitespace-nowrap mr-10 h-14 flex items-center pt-2 block opacity-50`,
-    active: `whitespace-nowrap mr-10 h-14 flex items-center pt-2 block opacity-100 border-b-2 border-yellow-300`,
-    main: `fixed top-12 left-0 bg-white border-b w-screen px-3 z-20 flex items-center overflow-x-scroll sm:hidden`,
+    tab: `whitespace-nowrap mr-7 h-14 flex items-center pt-2 block opacity-50`,
+    active: `whitespace-nowrap mr-7 h-14 flex items-center pt-2 block opacity-100 border-b-2 border-yellow-300`,
+    main: `no-scrollbar fixed top-12 left-0 bg-white border-b w-screen px-3 z-20 flex items-center overflow-x-auto sm:hidden`,
     fab: `shadow-2xl h-14 w-14 bg-blue-600 fixed bottom-0 mb-20 right-0 m-4 rounded-full flex justify-center items-center sm:hidden`,
 }
 
@@ -22,8 +22,8 @@ const EditTabs = () => {
             <li onClick={() => setActive(0)} className={viewCount === 0 ? styles.active : styles.tab}>👋 Introduction</li>
             <li onClick={() => setActive(1)} className={viewCount === 1 ? styles.active : styles.tab}>📷 Photos</li>
             <li onClick={() => setActive(2)} className={viewCount === 2 ? styles.active : styles.tab}>🔨 Your skills</li>
-            <li onClick={() => setActive(3)} className={viewCount === 3 ? styles.active : styles.tab}>🤩 Your socials</li>
             <li onClick={() => setActive(5)} className={viewCount === 5 ? styles.active : styles.tab}>💼 Projects</li>
+            <li onClick={() => setActive(3)} className={viewCount === 3 ? styles.active : styles.tab}>🤩 Your socials</li>
             <li onClick={() => setActive(4)} className={viewCount === 4 ? styles.active : styles.tab}>🎁 Receive tips</li>
         </ul>
 
