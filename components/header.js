6d621fb4d4b66a0e26ed239fb5ef48b6e50ecdc4
Langford Quarshie <langfordquarshie21@gmail.com>
2022-04-03 10:34:15 +0000

    ...

--- components/header.js
+++ components/header.js
@@ -6,8 +6,15 @@ import GhostButton from "./ghostButton"
 import Logo from "./logo"
 import Modal from "./modal"
 import { headerStyles } from "./styles/headerStyles"
+import { navStyles } from "./styles/navStyles"
 
 
+import graph from "../assets/svg/graph.svg"
+import brush from "../assets/svg/brush.svg"
+import theme from "../assets/svg/theme.svg"
+import settings from "../assets/svg/settings.svg"
+import shield from "../assets/svg/shield.svg"
+import coin from "../assets/svg/coin.svg"
 
 const Header = () => {
     const [route, setRoute] = useState('')
@@ -49,11 +56,49 @@ const Header = () => {
             showDrawer ?
                 <div className="fixed top-0 left-0 p-5 border-r border-b bg-white w-screen">
                     <p className="mb-5 font-bold" onClick={() => setShowDrawer(false)}>&times; close</p>
-                    <div className={headerStyles.drawerLink}><Link href="/">Dashboard</Link></div>
+
+                    <Link passHref={true} href="/">
+                        <p className={route === "/" ? navStyles.activeNavLink : navStyles.navLink}>
+                            <img className={navStyles.navLinkIcon} src={graph.src} />
+                            Dashboard
+                        </p>
+                    </Link>
+                    <Link passHref={true} href="/edit">
+                        <p className={route === "/edit" ? navStyles.activeNavLink : navStyles.navLink}>
+                            <img className={navStyles.navLinkIcon} src={brush.src} />
+                            Appearance
+                        </p>
+                    </Link>
+                    <Link passHref={true} href="/themes">
+                        <p className={route === "/themes" ? navStyles.activeNavLink : navStyles.navLink}>
+                            <img className={navStyles.navLinkIcon} src={theme.src} />
+                            Themes
+                        </p>
+                    </Link>
+                    <Link passHref={true} href="/">
+                        <p className={route === "/settings" ? navStyles.activeNavLink : navStyles.navLink}>
+                            <img className={navStyles.navLinkIcon} src={settings.src} />
+                            Settings
+                        </p>
+                    </Link>
+                    <Link passHref={true} href="/">
+                        <p className={route === "/upgrade" ? navStyles.activeNavLink : navStyles.navLink}>
+                            <img className={navStyles.navLinkIcon} src={shield.src} />
+                            Upgrade
+                        </p>
+                    </Link>
+                    <Link passHref={true} href="/">
+                        <p className={route === "/donate" ? navStyles.activeNavLink : navStyles.navLink}>
+                            <img className={navStyles.navLinkIcon} src={coin.src} />
+                            Donate
+                        </p>
+                    </Link>
+
+                    {/* <div className={headerStyles.drawerLink}><Link href="/">Dashboard</Link></div>
                     <div className={headerStyles.drawerLink}><Link href="/edit">Appearance</Link></div>
                     <div className={headerStyles.drawerLink}><Link href="/themes">Themes</Link></div>
                     <div className={headerStyles.drawerLink}><Link href="/upgrade">Upgrade</Link></div>
-                    <div className={headerStyles.drawerLink}><Link href="/donate">Donate</Link></div>
+                    <div className={headerStyles.drawerLink}><Link href="/donate">Donate</Link></div> */}
                 </div>
                 : <div></div>
         }
