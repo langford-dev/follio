2d793640e5e4c0ed5d49f3c1715e549d01b997c0
Langford Quarshie <langfordquarshie21@gmail.com>
2022-04-08 06:22:15 +0000

    ...

--- components/button.js
+++ components/button.js
@@ -1,5 +1,5 @@
 const Button = ({ label, onPress }) => {
-    return <div className="bg-dark text-light p-3 px-5 cursor-pointer hover:bg-light border w-max    whitespace-nowrap border-dark hover:text-dark rounded-md" onClick={onPress}>
+    return <div className="bg-dark text-light p-2 px-5 cursor-pointer hover:bg-light border w-max    whitespace-nowrap border-dark hover:text-dark rounded-md" onClick={onPress}>
         {label}
     </div>
     {/* <div
--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,9 @@
 import logo from "../assets/logo.png"
 
 const Logo = () => {
-    return <div className="bg-dark text-white w-10 h-10 sm:w-14 sm:h-14 flex items-center justify-center rounded-full pb-2">
+    return <div className="text-dark w-10 h-10 sm:w-14 sm:h-14 flex items-center justify-center rounded-full pb-2">
         <p className="font-bold sm:text-4xl text-2xl relative top-1 font-sans">f</p>
-        <div className="w-2 h-2 rounded-full bg-yellow-400 mt-6" />
+        <div className="w-2 h-2 rounded-full bg-light mt-6" />
     </div>
     // return <img src={logo.src} className="w-5" alt="logo" />
     // return <p className="font-medium text-2xl">
