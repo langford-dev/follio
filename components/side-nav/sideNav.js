698c45bd40e2dc31229503f6568bee710f1b7ea4
Langford Quarshie <langfordquarshie21@gmail.com>
2022-04-10 18:13:40 +0000

    added to product hunt and added pdct hunt banner

--- components/side-nav/sideNav.js
+++ components/side-nav/sideNav.js
@@ -16,11 +16,17 @@ const SideNav = () => {
 
         <div className={route === "/account/edit" ? "" : "hidden"}>
             <p className={navStyles.description} />
+
             <div className="pb-5">
                 <ViewsTracker />
             </div>
             <p className={navStyles.description}>{new Date().getFullYear()} &copy; Folio</p>
         </div>
+
+        <br />
+        <a href="https://www.producthunt.com/posts/follio?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-follio" target="_blank" rel="noreferrer">
+            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=340571&theme=light" alt="Follio - Easy&#0044;&#0032;no&#0045;code&#0032;and&#0032;customizable&#0032;portfolio&#0032;site&#0032;builder | Product Hunt" style={{ width: "250px", height: "50px" }} />
+        </a>
     </div >
 }
 
